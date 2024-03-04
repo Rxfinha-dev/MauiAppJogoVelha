e23177375459fc4471ca437fb5a2abf2ad0f31b5
Usuario <Usuario@CPD03-M05>
2024-03-04 15:00:01 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppJogoVelha/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace MauiAppJogoVelha
+{
+    public partial class MainPage : ContentPage
+    {
+        string vez = "O";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button disparador = (Button)sender;
+
+            disparador.IsEnabled = false;
+
+            if (vez=="O" ) 
+            {
+                disparador.Text = "O";
+                vez = "x";
+            }else
+            {
+                disparador.Text="X";
+                vez = "O"; 
+            }//Fecha Estrutura de Decisão
+
+        }
+    }//fecha a class MainPage
+
+}//fecha o namespace
