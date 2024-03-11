d32d3eb55b33fa7cb5c06ae5086cd0eb112d27ea
Usuario <Usuario@CPD03-M05>
2024-03-11 14:47:49 -0300

    reinicio de jogo

--- MauiAppJogoVelha/MainPage.xaml.cs
+++ MauiAppJogoVelha/MainPage.xaml.cs
@@ -5,7 +5,7 @@
         string vez = "O";
 
         public MainPage()
-        {
+        { 
             InitializeComponent();
         }
 
@@ -32,74 +32,89 @@
             if (btn10.Text == "O" && btn11.Text == "O" && btn12.Text == "O")//Colunas com vencedor O 
                 {
                 DisplayAlert("Você Ganhou","Parabéns","Fechar");
-                    
+                limpar();
                  }
             else if(btn20.Text=="O" && btn21.Text == "O" && btn22.Text == "O")
             {
                 DisplayAlert("Você Ganhou", "Parabéns", "Fechar");
+                limpar();
             }
             else if (btn30.Text == "O" && btn31.Text == "O" && btn32.Text == "O")
             {
                 DisplayAlert("Você Ganhou", "Parabéns", "Fechar");
+                limpar();
             }
             //linhas com vencedor O
             else if (btn10.Text == "O" && btn20.Text == "O" && btn30.Text == "O")
             {
                 DisplayAlert("Você Ganhou", "Parabéns", "Fechar");
+                limpar();
             }
             else if (btn11.Text == "O" && btn21.Text == "O" && btn31.Text == "O")
             {
                 DisplayAlert("Você Ganhou", "Parabéns", "Fechar");
+                limpar();
             }
             else if (btn12.Text == "O" && btn22.Text == "O" && btn32.Text == "O")
             {
                 DisplayAlert("Você Ganhou", "Parabéns", "Fechar");
+                limpar();
             }
             //diagonais com vencedor O
             else if (btn10.Text == "O" && btn21.Text == "O" && btn32.Text == "O")
             {
                 DisplayAlert("Você Ganhou", "Parabéns", "Fechar");
+                limpar();
             }
             else if (btn12.Text == "O" && btn21.Text == "O" && btn30.Text == "O")
             {
                 DisplayAlert("Você Ganhou", "Parabéns", "Fechar");
+                limpar();
             }//Fecha lógica de ganhador O
 
             //Inicio Lógica de ganhador X
             if (btn10.Text == "X" && btn11.Text == "X" && btn12.Text == "X")//Colunas com vencedor X
             {
                 DisplayAlert("Você Ganhou", "Parabéns", "Fechar");
+                limpar();
 
             }
             else if (btn20.Text == "X" && btn21.Text == "X" && btn22.Text == "X")
             {
                 DisplayAlert("Você Ganhou", "Parabéns", "Fechar");
+                limpar();
             }
             else if (btn30.Text == "X" && btn31.Text == "X" && btn32.Text == "X")
             {
                 DisplayAlert("Você Ganhou", "Parabéns", "Fechar");
+                limpar();
             }
             //linhas com vencedor X
             else if (btn10.Text == "X" && btn20.Text == "X" && btn30.Text == "X")
             {
                 DisplayAlert("Você Ganhou", "Parabéns", "Fechar");
+                limpar();
             }
             else if (btn11.Text == "X" && btn21.Text == "X" && btn31.Text == "X")
             {
                 DisplayAlert("Você Ganhou", "Parabéns", "Fechar");
+                limpar();
             }
             else if (btn12.Text == "X" && btn22.Text == "X" && btn32.Text == "X")
             {
                 DisplayAlert("Você Ganhou", "Parabéns", "Fechar");
+                limpar();
             }
             //diagonais com vencedor X
             else if (btn10.Text == "X" && btn21.Text == "X  " && btn32.Text == "X")
             {
                 DisplayAlert("Você Ganhou", "Parabéns", "Fechar");
+                limpar();
             }
             else if (btn12.Text == "X" && btn21.Text == "X" && btn30.Text == "X")
             {
                 DisplayAlert("Você Ganhou", "Parabéns", "Fechar");
+                limpar();
             }//Fecha lógica de ganhador X
             //lógica de resultado velha
             else if(btn10.IsEnabled == false && btn11.IsEnabled == false &&btn12.IsEnabled == false &&
@@ -111,41 +126,33 @@
             btn32.IsEnabled == false)
             {
                 DisplayAlert("Deu Velha", "Que Pena!", "Fechar");
+                limpar();
             }
 
 
 
 
         }
-
-        private void ResetButton_Clicked(object sender, EventArgs e)
-        {
-            // Este é o manipulador de eventos para o botão "Reset"
-            // Aqui você reinicia o estado do jogo
-
-            // Reinicializa o estado dos botões
+        private void limpar() {
             btn10.Text = "";
-            btn11.Text = "";
-            btn12.Text = "";
-            btn20.Text = "";
-            btn21.Text = "";
-            btn22.Text = "";
-            btn30.Text = "";
-            btn31.Text = "";
-            btn32.Text = "";
-
-            // Habilita todos os botões
             btn10.IsEnabled = true;
+            btn11.Text = "";
             btn11.IsEnabled = true;
+            btn12.Text = "";
             btn12.IsEnabled = true;
+            btn20.Text = "";
             btn20.IsEnabled = true;
-            btn21.IsEnabled = true;
+            btn21.Text = "";
+            btn21.IsEnabled = true; 
+            btn22.Text = "";
             btn22.IsEnabled = true;
+            btn30.Text = "";
             btn30.IsEnabled = true;
+            btn31.Text = "";
             btn31.IsEnabled = true;
+            btn32.Text = "";
             btn32.IsEnabled = true;
 
-            vez = "O"; // Define a vez para o jogador "O"
         }
     }//fecha a class MainPage
 
